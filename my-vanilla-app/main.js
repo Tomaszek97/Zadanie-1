5fe22c07b3ea5638374d6637d471890e7f0150f2
tomek-parfian <tomekparfian97@gmail.com>
2023-08-16 16:32:27 +0200

    Adding the created changes

--- my-vanilla-app/main.js
+++ my-vanilla-app/main.js
@@ -1,13 +1,50 @@
 import './style.css'
-const apiUrl = 'https://api-eko-bazarek.azurewebsites.net/api/products/types'
-var buttonGet = document.getElementById('Types-item')
+document.addEventListener('DOMContentLoaded', function () {
+	// Znajdź listy elementów na stronie
+	const typesList = document.querySelector('.TypesList-root')
+	const categoriesList = document.querySelector('.CategoriesList-root')
+	const productsContainer = document.querySelector('.Products-root')
 
-buttonGet.addEventListener('click', () => {
-	getTypes().then(types => console.log(types))
+	// Pobierz rodzaje produktów i wyświetl je na stronie
+	fetch('https://api-eko-bazarek.azurewebsites.net/api/products/types')
+		.then(function (response) {
+			return response.json()
+		})
+		.then(function (types) {
+			types.sort(function (a, b) {
+				return a.name.localeCompare(b.name)
+			})
+
+			types.forEach(function (type) {
+				const typeItem = document.createElement('li')
+				typeItem.classList.add('Types-item')
+				typeItem.textContent = type.name
+				typesList.appendChild(typeItem)
+			})
+		})
+
+	// Pobierz kategorie produktów i wyświetl je na stronie
+	fetch('https://api-eko-bazarek.azurewebsites.net/api/products/categories')
+		.then(function (response) {
+			return response.json()
+		})
+		.then(function (categories) {
+			categories.sort(function (a, b) {
+				return a.name.localeCompare(b.name)
+			})
+
+			categories.forEach(function (category) {
+				const categoryItem = document.createElement('li')
+				categoryItem.textContent = category.name
+
+				if (category.iconUrl) {
+					const icon = document.createElement('img')
+					icon.alt = category.name + ' icon'
+					icon.src = category.iconUrl
+					categoryItem.appendChild(icon)
+				}
+
+				categoriesList.appendChild(categoryItem)
+			})
+		})
 })
-
-async function getTypes() {
-	const respons = await fetch(apiUrl)
-	return respons.json()
-}
-
