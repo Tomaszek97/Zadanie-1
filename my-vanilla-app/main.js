d608efbf380c569fa796ea8bc882b9a7509b8ff9
tomek-parfian <tomekparfian97@gmail.com>
2023-08-16 12:05:17 +0200

    Adding the created changes

--- my-vanilla-app/main.js
+++ my-vanilla-app/main.js
@@ -1,38 +1,13 @@
 import './style.css'
 const apiUrl = 'https://api-eko-bazarek.azurewebsites.net/api/products/types'
-var buttonGet = document.getElementById('btnGet')
+var buttonGet = document.getElementById('Types-item')
 
-// buttonGet.addEventListener('click', () => {
-// 	getTypes().then(types => console.log(types))
-// })
+buttonGet.addEventListener('click', () => {
+	getTypes().then(types => console.log(types))
+})
 
 async function getTypes() {
 	const respons = await fetch(apiUrl)
 	return respons.json()
 }
 
-const jsonData = [
-  {
-    "id": "APPLE",
-    "name": "Jabłka",
-    "type": "FRUITS",
-    "iconUrl": "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-apple-64.png"
-  },
-  {
-    "id": "BEEF",
-    "name": "Wołowina",
-    "type": "MEAT",
-    "iconUrl": "https://api-eko-bazarek.azurewebsites.net/images/categories/icons8-beef-64.png"
-  }
-];
-
-const listContainer = document.getElementById('list-container'); 
-
-const itemsHtml = jsonData.map(item => `
-  <div class="list-item">
-    <img src="${item.iconUrl}" alt="${item.name}">
-    <div>${item.name}</div>
-  </div>
-`).join('');
-
-listContainer.innerHTML = itemsHtml;
