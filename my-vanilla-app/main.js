76d9b93506d222c21e6a79c145ae940ee24ad156
tomek-parfian <tomekparfian97@gmail.com>
2023-08-19 00:59:50 +0200

    addition of new changes

--- my-vanilla-app/main.js
+++ my-vanilla-app/main.js
@@ -1,57 +1,66 @@
 import './style.css'
+
+let typesData = []
+let categoriesData = []
+
 document.addEventListener('DOMContentLoaded', function () {
-	// Znajdź listy elementów na stronie
-	const typesList = document.querySelector('.TypesList-root')
-	const categoriesList = document.querySelector('.CategoriesList-root')
-	const productsContainer = document.querySelector('.Products-root')
+	const typesList = document.querySelector('.typesList-root')
+	const categoriesList = document.querySelector('.categoriesList-root')
 
-	// Pobierz rodzaje produktów i wyświetl je na stronie
+	function createTypeElement(type) {
+		const typeItem = document.createElement('li')
+		const buttonItem = document.createElement('button')
+		buttonItem.classList.add('btn-type-item')
+		buttonItem.setAttribute('id', type.id)
+		typeItem.classList.add('types-item')
+		typeItem.textContent = type.name
+		typeItem.appendChild(buttonItem)
+		typesList.appendChild(typeItem)
+	}
+
+	function createCategoryElement(category) {
+		const categoryItem = document.createElement('li')
+		categoryItem.textContent = category.name
+
+		if (category.iconUrl) {
+			const icon = document.createElement('img')
+			icon.alt = category.name + ' icon'
+			icon.src = category.iconUrl
+			categoryItem.appendChild(icon)
+		}
+
+		categoriesList.appendChild(categoryItem)
+	}
+
 	fetch('https://api-eko-bazarek.azurewebsites.net/api/products/types')
-		.then(function (response) {
-			return response.json()
-		})
-		.then(function (types) {
-			types.sort(function (a, b) {
-				return a.name.localeCompare(b.name)
-			})
+		.then(response => response.json())
+		.then(types => {
+			typesData = types
 
-			types.forEach(function (type) {
-				const typeItem = document.createElement('li')
-				const buttonItem = document.createElement('button')
-				buttonItem.classList.add('btn-type-item')
-				buttonItem.setAttribute('id', 'button-type' + type.id)
-				typeItem.classList.add('Types-item')
-				typeItem.textContent = type.name
-				typeItem.appendChild(buttonItem)
-				typesList.appendChild(typeItem)
-			})
+			types.sort((a, b) => a.name.localeCompare(b.name))
+
+			types.forEach(createTypeElement)
 		})
 
-	// Pobierz kategorie produktów i wyświetl je na stronie
 	fetch('https://api-eko-bazarek.azurewebsites.net/api/products/categories')
-		.then(function (response) {
-			return response.json()
+		.then(response => response.json())
+		.then(categories => {
+			categoriesData = categories
+
+			categories.sort((a, b) => a.name.localeCompare(b.name))
+
+			categories.forEach(createCategoryElement)
 		})
-		.then(function (categories) {
-			categories.sort(function (a, b) {
-				return a.name.localeCompare(b.name)
-			})
 
-			categories.forEach(function (category) {
-				const categoryItem = document.createElement('li')
-				categoryItem.textContent = category.name
+	const buttons = document.getElementsByClassName('btn-type-item')
+	for (let i = 0; i < buttons.length; i++) {
+		const button = buttons[i]
+		button.addEventListener('click', function () {
+			const typeId = button.getAttribute('id')
+			const filteredCategories = categoriesData.filter(category => category.typeId === typeId)
 
-				if (category.iconUrl) {
-					const icon = document.createElement('img')
-					icon.alt = category.name + ' icon'
-					icon.src = category.iconUrl
-					categoryItem.appendChild(icon)
-				}
-
-				categoriesList.appendChild(categoryItem)
-			})
+			categoriesList.innerHTML = ''
+			filteredCategories.forEach(createCategoryElement)
 		})
+	}
 })
-
-const buttons = document.getElementsByClassName('btn-type-item')
-console.table(buttons)
